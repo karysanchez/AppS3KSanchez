5ebe76e5a5d4d940d3bdaba953eb220024341370
KaRoT <KaRoT@DESKTOP-NMO8M99>
2021-10-19 19:31:37 -0500

    Agregar archivos de proyecto.

---
+++ AppS3KSanchez/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS3KSanchez
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //async para no consumir muchos recuros abre la una y cierra la otra y se debe usar desde ahí para cada accion el await
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //Almacenando en variables lo que el usuario ingresa
+                string usuario = txtUsuario.Text;
+                string clave = txtClave.Text;
+
+                //Permite abrir la ventana (viewDos)
+                await Navigation.PushAsync(new viewDos(usuario, clave));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Mensaje de advertencia", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ AppS3KSanchez/viewDos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS3KSanchez
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viewDos : ContentPage
+    {
+        public viewDos(string usuario, string clave)
+        {
+            InitializeComponent();
+            //se le carga los datos para mostrar
+            lblUsuario.Text = usuario;
+            lblClave.Text = clave;
+
+            txtUsuario.Text = usuario;
+            txtClave.Text = clave;
+        }
+    }
+}
